b11e36613ab00b28660f3cefb3dedc0ac96e9ade
Sascha Raabe <sascha_develop@gmx.de>
2020-02-19 23:11:54 +0100

    zshrc added

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ln -s $DIR/.vimrc ~/.vimrc
 ln -s $DIR/.vimfiles ~/.vimfiles
 ln -s $DIR/.tmux.conf ~/.tmux.conf
+ln -s $DIR/.zshrc ~/.zshrc
 
 # .vim dir anlegen
 if [ ! -d "~/.vim/" ];
